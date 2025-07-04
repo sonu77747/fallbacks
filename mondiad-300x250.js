dd6d089f31bbc4c53ee4ed9a3a91635804754fe0
sonu77747 <healthcaretips.hub2025@gmail.com>
2025-07-04 20:01:07 +0530

    Create mondiad-300x250.js

---
+++ mondiad-300x250.js
@@ -0,0 +1,8 @@
+var mDiv = document.createElement("div");
+mDiv.setAttribute("data-mndazid", "c0e9d66e-0f7b-472d-a787-00185494aceb");
+document.body.appendChild(mDiv);
+
+var mScript = document.createElement("script");
+mScript.src = "https://ss.mrmnd.com/native.js";
+mScript.async = true;
+document.body.appendChild(mScript);
